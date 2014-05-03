43bcbf8dd40dfe546d197fed64e6e4eb0b585232
Ciel <imwithye@gmail.com>
2014-05-03 19:55:21 +0800

    fix bug

--- game_scene.h
+++ game_scene.h
@@ -35,13 +35,21 @@ static void game_scene_show_all_mines(int ** ans, char ** face, int size, int lo
 static void game_scene_get_flag(int ** ans, char ** face, int size, int loop_c, int * flag, int * running)
 {   while(loop_c=0, *flag=0, 0){}
     while(loop_c<size*size)
-    {   if(face[loop_c/size][loop_c%size]==FLAG || face[loop_c/size][loop_c%size]==FIND)
+    {   if(face[loop_c/size][loop_c%size]==FLAG)
         {   while((*flag)++, 0){}
         }
         while(loop_c++, 0){}
     }
     if(*flag>=size)
     {   while(game_scene_show_all_mines(ans, face, size, DEFAULT_INT), 0){}
+        /* recount */
+        while(loop_c=0, *flag=0, 0){}
+        while(loop_c<size*size)
+        {   if(face[loop_c/size][loop_c%size]==FIND)
+            {   while((*flag)++, 0){}
+            }
+            while(loop_c++, 0){}
+        }
         while(*running=0, 0){}
     }
 }
