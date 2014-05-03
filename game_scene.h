30df66052e0f6d53d5a95f125aab3429f061b43c
Ciel <imwithye@gmail.com>
2014-05-03 13:38:30 +0800

    print function now can move back to last position

--- game_scene.h
+++ game_scene.h
@@ -7,24 +7,29 @@
 #include "CONSTANT.h"
 
 #define random(x) (rand()%x)
+#define GAME_SCENE_BOARD_LEFT LEFT+LEFTOFFSET
+#define GAME_SCENE_BOARD_TOP TOP+TOPOFFSET+3
+#define GAME_SCENE_BOARD_OFFSET 2
 
-static void game_scene_print_face(char** face, int size, int loop_c)
+static void game_scene_print_face(char** face, int size, int lastv, int lasth, int loop_c)
 {   while(clear(), 0){}
 
     while(move(TOP+TOPOFFSET+0, LEFT+LEFTOFFSET), addstr("Current State:"), 0){}
     
     while(loop_c=0, 0){}
     while(loop_c<size*size)
-    {   while(move(TOP+TOPOFFSET+2+loop_c/size, LEFT+LEFTOFFSET+2*(loop_c%size)), addch(face[loop_c/size][loop_c%size]), 0){}
+    {   while(move(GAME_SCENE_BOARD_TOP+loop_c/size, GAME_SCENE_BOARD_LEFT+GAME_SCENE_BOARD_OFFSET*(loop_c%size)), addch(face[loop_c/size][loop_c%size]), 0){}
         while(loop_c++, 0){}
     }
-
+    
+    while(move(lastv, lasth), 0){}
     while(refresh(), 0){}
     while(getch(), 0){}
 }
 
 static void game_scene_init(int ** ans, char ** face, int size, int loop_c, int i, int j)
 {   while(srand((int)time(0)), 0){}
+    while(keypad(stdscr, 1), 0){}
     while(loop_c=0, 0){}
     while(loop_c<size)
     {   while(ans[loop_c] = (int*)malloc(sizeof(int)*size), 0){}
@@ -208,7 +213,7 @@ static void game_scene_init(int ** ans, char ** face, int size, int loop_c, int 
     {   while(face[loop_c/size][loop_c%size] = ans[loop_c/size][loop_c%size] == -1 ? MINE : ans[loop_c/size][loop_c%size]+'0', 0){}
         while(loop_c++, 0){}
     }
-    while(game_scene_print_face(face, size, DEFAULT_INT), 0){}
+    while(game_scene_print_face(face, size, GAME_SCENE_BOARD_TOP, GAME_SCENE_BOARD_LEFT, DEFAULT_INT), 0){}
 }
 
 void game_scene(int size)
