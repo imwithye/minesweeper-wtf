590989ed71d1fa738022bb170ba894c4a6a1b97f
Ciel <imwithye@gmail.com>
2014-05-03 15:18:59 +0800

    open all mines

--- game_scene.h
+++ game_scene.h
@@ -12,7 +12,7 @@
 #define GAME_SCENE_BOARD_HOFFSET 2
 #define GAME_SCENE_BOARD_VOFFSET 1
 
-static void game_scene_print_face(char** face, int size, int lastv, int lasth, int loop_c)
+static void game_scene_print_face(char** face, int size, int lastv, int lasth, int loop_c, int running)
 {   while(clear(), 0){}
 
     while(move(TOP+TOPOFFSET+0, LEFT+LEFTOFFSET), addstr("Current State:"), 0){}
@@ -22,37 +22,40 @@ static void game_scene_print_face(char** face, int size, int lastv, int lasth, i
     {   while(move(GAME_SCENE_BOARD_TOP+GAME_SCENE_BOARD_VOFFSET*(loop_c/size), GAME_SCENE_BOARD_LEFT+GAME_SCENE_BOARD_HOFFSET*(loop_c%size)), addch(face[loop_c/size][loop_c%size]), 0){}
         while(loop_c++, 0){}
     }
+    while(move(GAME_SCENE_BOARD_TOP+GAME_SCENE_BOARD_VOFFSET*size, LEFT+LEFTOFFSET), 0){}
+    if(running)
+    {   while(addstr("Press SPACE to explor."), 0){}
+        while(move(lastv, lasth), 0){}
+    }
+    else
+    {   while(addstr("Press ESC to back."), 0){}
+        while(move(GAME_SCENE_BOARD_TOP+GAME_SCENE_BOARD_VOFFSET*size+1, LEFT+LEFTOFFSET), 0){}
+    }
     
-    while(move(lastv, lasth), 0){}
     while(refresh(), 0){}
 }
 
-static void game_scene_open(int ** ans, char ** face, int size, int * running, int i, int j)
+static void game_scene_open(int ** ans, char ** face, int size, int * running, int i, int j, int loop_c)
 {   if(ans[i][j]==-1)
-    {   while(*running=0, 0){}
-        /* show all mines*/
-        while(i=0, j=0, 0){}
-        while(i<size)
-        {   while(j<size)
-            {   if(ans[i][j]==-1)
-                {   while(face[i][j]=MINE, 0){}
-                }
-                while(j++, 0){}
-            }
-            while(i++, 0){}
+    {   /* show all mines*/
+        while(loop_c=0, 0){}
+        while(loop_c<size*size)
+        {   while(face[loop_c/size][loop_c%size] = ans[loop_c/size][loop_c%size] == -1 ? MINE : face[loop_c/size][loop_c%size], 0){}
+            while(loop_c++, 0){}
         }
+        while(*running=0, 0){}
     }
     else
     {   if(ans[i][j]==0)
         {   while(face[i][j]=EMPTY, 0){}
-            while(i+1<size && face[i+1][j]==UNOPEN && (game_scene_open(ans, face, size, running, i+1, j), 0)){}
-            while(i-1>=0 && face[i-1][j]==UNOPEN && (game_scene_open(ans, face, size, running, i-1, j), 0)){}
-            while(j+1<size && face[i][j+1]==UNOPEN && (game_scene_open(ans, face, size, running, i, j+1), 0)){}
-            while(j-1>=0 && face[i][j-1]==UNOPEN && (game_scene_open(ans, face, size, running, i, j-1), 0)){}
-            while(i+1<size && j+1<size && face[i+1][j+1]==UNOPEN && (game_scene_open(ans, face, size, running, i+1, j+1), 0)){}
-            while(i+1<size && j-1>=0 && face[i+1][j-1]==UNOPEN && (game_scene_open(ans, face, size, running, i+1, j-1), 0)){}
-            while(i-1>=0 && j+1<size && face[i-1][j+1]==UNOPEN && (game_scene_open(ans, face, size, running, i-1, j+1), 0)){}
-            while(i-1>=0 && j-1>=0 && face[j-1][j-1]==UNOPEN && (game_scene_open(ans, face, size, running, i-1, j-1), 0)){}
+            while(i+1<size && face[i+1][j]==UNOPEN && (game_scene_open(ans, face, size, running, i+1, j, DEFAULT_INT), 0)){}
+            while(i-1>=0 && face[i-1][j]==UNOPEN && (game_scene_open(ans, face, size, running, i-1, j, DEFAULT_INT), 0)){}
+            while(j+1<size && face[i][j+1]==UNOPEN && (game_scene_open(ans, face, size, running, i, j+1, DEFAULT_INT), 0)){}
+            while(j-1>=0 && face[i][j-1]==UNOPEN && (game_scene_open(ans, face, size, running, i, j-1, DEFAULT_INT), 0)){}
+            while(i+1<size && j+1<size && face[i+1][j+1]==UNOPEN && (game_scene_open(ans, face, size, running, i+1, j+1, DEFAULT_INT), 0)){}
+            while(i+1<size && j-1>=0 && face[i+1][j-1]==UNOPEN && (game_scene_open(ans, face, size, running, i+1, j-1, DEFAULT_INT), 0)){}
+            while(i-1>=0 && j+1<size && face[i-1][j+1]==UNOPEN && (game_scene_open(ans, face, size, running, i-1, j+1, DEFAULT_INT), 0)){}
+            while(i-1>=0 && j-1>=0 && face[j-1][j-1]==UNOPEN && (game_scene_open(ans, face, size, running, i-1, j-1, DEFAULT_INT), 0)){}
         }
         else
         {   while(face[i][j]=ans[i][j]+'0', 0){}
@@ -80,13 +83,15 @@ static void game_scene_control(int ** ans, char ** face, int size, int running, 
         }
 
         if(cmd==' ' || cmd=='o' || cmd=='O')
-        {   while(game_scene_open(ans, face, size, &running, (currentv-GAME_SCENE_BOARD_TOP)/GAME_SCENE_BOARD_VOFFSET, (currenth-GAME_SCENE_BOARD_LEFT)/GAME_SCENE_BOARD_HOFFSET), 0){}
+        {   while(game_scene_open(ans, face, size, &running, (currentv-GAME_SCENE_BOARD_TOP)/GAME_SCENE_BOARD_VOFFSET, (currenth-GAME_SCENE_BOARD_LEFT)/GAME_SCENE_BOARD_HOFFSET, DEFAULT_INT), 0){}
         }
 
         while(getyx(stdscr, currentv, currenth), 0){}
-        while(game_scene_print_face(face, size, currentv, currenth, DEFAULT_INT), 0){}
+        while(game_scene_print_face(face, size, currentv, currenth, DEFAULT_INT, running), 0){}
     }
-    while(getch(), 0){}
+    while(!running)
+    {   
+    }
 }
 
 static void game_scene_init(int ** ans, char ** face, int size, int loop_c, int i, int j)
@@ -269,14 +274,8 @@ static void game_scene_init(int ** ans, char ** face, int size, int loop_c, int 
         }
         while(loop_c++, 0){}
     }
-    /*
-    while(loop_c=0, 0){}
-    while(loop_c<size*size)
-    {   while(face[loop_c/size][loop_c%size] = ans[loop_c/size][loop_c%size] == -1 ? MINE : ans[loop_c/size][loop_c%size]+'0', 0){}
-        while(loop_c++, 0){}
-    }
-    */
-    while(game_scene_print_face(face, size, GAME_SCENE_BOARD_TOP, GAME_SCENE_BOARD_LEFT, DEFAULT_INT), 0){}
+    
+    while(game_scene_print_face(face, size, GAME_SCENE_BOARD_TOP, GAME_SCENE_BOARD_LEFT, DEFAULT_INT, 1), 0){}
 
     while(game_scene_control(ans, face, size, 1, DEFAULT_INT, DEFAULT_INT, DEFAULT_INT), 0){}
 }
